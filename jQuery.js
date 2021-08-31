e2f4a67aba9ec984366674b735a18c0636211b1e
nami73 <73630181+kashimanami@users.noreply.github.com>
2021-08-31 15:08:19 +0900

    Add files via upload

---
+++ jQuery.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function(){
+												$('.slider').not('.slick-initialized').slick({
+													dots:true,
+													autoplay: false,
+													autoplaySpeed: 3000,
+												});
+						});
